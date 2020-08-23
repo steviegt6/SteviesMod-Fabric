36d85d702753ff2c5219702b4a520830c7ea267b
Stevie <xxlennygamerxx@gmail.com>
2020-08-22 22:24:26 -0700

    Changing names :ahh:

---
+++ src/main/java/io/github/steviegt6/SteviesMod.java
@@ -0,0 +1,10 @@
+package io.github.steviegt6;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SteviesMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("SteviesMod initialized.");
+	}
+}
